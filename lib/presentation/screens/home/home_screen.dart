b4a9d3e1b0dc1e24a9b73c9dd6379fbc5741884b
Guillermo-NH <ingnanco@gmail.com>
2023-06-07 23:32:57 -0600

    navegacion entre pantallas

--- lib/presentation/screens/home/home_screen.dart
+++ lib/presentation/screens/home/home_screen.dart
@@ -47,7 +47,9 @@ class _CustomListTile extends StatelessWidget {
       subtitle: Text(menuItem.subTitle),
       leading: Icon(menuItem.icon, color: colors.primary),
       trailing: Icon(Icons.arrow_forward_ios_rounded, color: colors.primary),
-      onTap: () {},
+      onTap: () {
+        Navigator.pushNamed(context, menuItem.link);
+      },
     );
   }
 }
