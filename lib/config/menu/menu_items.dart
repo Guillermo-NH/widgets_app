af92875b716f89e5819a8e3ff554c7a0e1086a07
Guillermo-NH <ingnanco@gmail.com>
2023-06-11 23:40:59 -0600

    progress indicator

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -23,5 +23,10 @@ const appMenuItems = <MenuItem>[
       title: 'Tarjetas',
       subTitle: 'Uncontenedor estilizado',
       link: '/cards',
-      icon: Icons.credit_card)
+      icon: Icons.credit_card),
+  MenuItem(
+      title: 'ProgressIndicator',
+      subTitle: 'Generales y controlados',
+      link: '/progress',
+      icon: Icons.refresh_rounded),
 ];
